e9e6b3b5002695af50fbe5338b598be21a169c4b
Will Dixon <will.k.dixon@gmail.com>
2020-01-27 18:44:23 -0700

    btree level order traversal

---
+++ java/leetcode/src/leetcode/recursion_to_iteration/btree_levelorder_traversal/SolutionRecursive.java
@@ -0,0 +1,45 @@
+package leetcode.recursion_to_iteration.btree_levelorder_traversal;
+
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    List<List<Integer>> list = new ArrayList<>(); // ArrayList for O(1) lookup
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        helper(root, 0);
+
+        return list;
+    }
+
+    private void helper(TreeNode node, int level) {
+        if (node == null)
+            return;
+
+        if (list.size() - 1 < level) {
+            list.add(new LinkedList<>());
+        }
+
+        list.get(level).add(node.val);
+
+        helper(node.left, level + 1);
+        helper(node.right, level + 1);
+
+    }
+}
