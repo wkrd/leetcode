45165375be7362414b26db31ca2a8718f3310f0e
Will Dixon <will.k.dixon@gmail.com>
2020-02-01 15:33:33 -0700

    largest rectangle in histogram (divide & conquer)

---
+++ java/leetcode/src/leetcode/recursion/largest_rectangle_in_histogram/Solution.java
@@ -0,0 +1,39 @@
+package leetcode.recursion.largest_rectangle_in_histogram;
+
+class Solution {
+    private int maxArea = 0;
+
+    public int largestRectangleArea(int[] heights) {
+        dc(heights, 0, heights.length - 1);
+
+        return maxArea;
+    }
+
+    public void dc(int[] heights, int left, int right) {
+        if (left < 0 || right > heights.length - 1)
+            return;
+        if (left > right)
+            return;
+
+        int minHeightIndex = left;
+        for (int i = left; i < right; i++) {
+            if (heights[i] < heights[minHeightIndex]) {
+                minHeightIndex = i;
+            }
+        }
+
+        int area = heights[minHeightIndex] * (1 + right - left);
+        if (area > maxArea) {
+            maxArea = area;
+        }
+
+        dc(heights, left, minHeightIndex - 1);
+        dc(heights, minHeightIndex + 1, right);
+    }
+
+    public static void main(String[] args) {
+        int area = new Solution().largestRectangleArea(new int[] { 1, 2, 4, 3 });
+
+        System.out.println(area);
+    }
+}
