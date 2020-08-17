e5fe587d2d9dc98bbd9c399d1b31e73759a463bb
Will Dixon <will.k.dixon@gmail.com>
2020-08-16 18:56:56 -0600

    Find Numbers with Even Number of Digits

---
+++ ruby/find_nums_with_even_digit_count.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def find_numbers(nums)
+  even_count = 0
+  nums.each do |num|
+    digits = 0
+    loop do
+      digits += 1
+      num = num / 10
+      break if num == 0
+    end
+    even_count += 1 if digits % 2 == 0
+  end
+  even_count
+end
